b1f431d97b6d1e0aa4ef7788073a6f7f121f5558
Paul <pauldro952@outlook.com>
2022-04-25 16:02:00 -0500

    Create Tsv File reader

---
+++ lib/PhpSpreadsheet/Reader/Tsv.php
@@ -0,0 +1,45 @@
+<?php namespace Lib\PhpSpreadsheet\Reader;
+// PHP Spreadsheet
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Csv as Reader;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+
+use Lib\PhpSpreadsheet\StringValueBinder;
+
+/**
+ * Reader\Tsv
+ * 
+ * Handles Getting the Tab-separated Reader, Returing Spreadsheets
+ */
+class Tsv {
+	private static $reader;
+
+	/**
+	 * Return Reader
+	 * @return Reader
+	 */
+	public static function reader() {
+		if (empty(self::$reader)) {
+			$reader = new Reader();
+			$reader->setInputEncoding('CP1252');
+			$reader->setSheetIndex(0);
+			$reader->setDelimiter("\t");
+			self::$reader = $reader;
+		}
+		return self::$reader;
+	}
+
+	/**
+	 * Use Reader to return Spreadsheet
+	 *
+	 * @param  string $filepath  /path/to/file
+	 * @return Spreadsheet
+	 */
+	public static function getSpreadsheet($filepath) {
+		Cell::setValueBinder(new StringValueBinder());
+
+		$reader = self::reader();
+		$reader->setReadDataOnly(true);
+		return $reader->load($filepath);
+	}
+}
