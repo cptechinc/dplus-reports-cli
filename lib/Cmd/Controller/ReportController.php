e8fc579722d10af8f9377d55b2f9fe3ae4998f66
Paul <pauldro952@outlook.com>
2022-04-26 11:12:00 -0500

    Update ReportController.php

--- lib/Cmd/Controller/ReportController.php
+++ lib/Cmd/Controller/ReportController.php
@@ -1,4 +1,5 @@
 <?php namespace Lib\Cmd\Controller;
+use Lib\Files;
 // Dplus Reports
 use Lib\Reports\Report;
 
@@ -79,5 +80,35 @@ abstract class ReportController extends JsonController {
 	protected function initReport() {
 		$this->report = $this->getReport();
 		return empty($this->report) === false;
+	}
+
+	/**
+	 * Parse Command to Copy Spreedsheet to Location
+	 * @return bool
+	 */
+	protected function copySpreadsheetFromCmd() {
+		if (empty($this->lastWrittenFile)) {
+			return $this->error('Written File not found');
+		}
+		if (file_exists($this->lastWrittenFile) === false) {
+			return $this->error("File '$this->lastWrittenFile' not found");
+		}
+		if ($this->hasParam('dir') === false || is_dir($this->getParam('dir')) === false) {
+			return $this->error("Invalid directory provided (dir=DIR)");
+		}
+		$copier = new Files\Copier();
+		$copier->setOriginalFilepath($this->lastWrittenFile);
+		$copier->setDestinationDirectory($this->getParam('dir'));
+
+		if ($this->hasParam('filename')) {
+			$copier->setDestinationFilename($this->getParam('filename'));
+		}
+
+		if ($copier->copy() === false) {
+			return $this->error($copier->error);
+		}
+		$this->getPrinter()->success("Copied File: $copier->lastCopyFile");
+		$this->lastWrittenFile = $copier->lastCopyFile;
+		return true;
 	}
 }
