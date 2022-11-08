6740d1253303ec74d9a9bceebad8c7f66303267b
Paul <pauldro952@outlook.com>
2022-11-08 11:44:35 -0600

    FIX: handle thousands comma

--- lib/PhpSpreadsheet/DataTypes/Number.php
+++ lib/PhpSpreadsheet/DataTypes/Number.php
@@ -1,6 +1,7 @@
 <?php namespace Lib\PhpSpreadsheet\DataTypes;
 // PhpSpreadsheet
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 
 /**
@@ -14,19 +15,31 @@ class Number {
 	
 	/**
 	 * Return Format Code from number
-	 * @param  string|float $nbr
+	 * @param  float|int|string $nbr
 	 * @return string
 	 */
 	public static function generateFormatCode($nbr) {
 		$nbr = "$nbr";
-		$formatCode = "0";
-		
+		$formatCode = rtrim(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1, '.00');
+		$decimalFormatCode  = self::generateDecimalFormatCode($nbr);
+		return $formatCode . $decimalFormatCode;
+	}
+
+	/**
+	 * Return Format Code for Decimal Seperator
+	 * @param  float|int|string $nbr
+	 * @return string
+	 */
+	public static function generateDecimalFormatCode($nbr) {
+		$nbr = "$nbr";
+
 		if (strpos($nbr, '.') !== false) {
 			$parts = explode('.', $nbr);
 			$beforeDecimal = str_pad("", strlen($parts[0]), "0");
 			$afterDecimal = str_pad("", strlen($parts[1]), "0");
 			$formatCode = "." . $afterDecimal;
+			return $formatCode;
 		}
-		return $formatCode;
+		return '';
 	}
 }
