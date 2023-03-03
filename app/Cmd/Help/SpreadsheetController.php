3f7bb815f180b6e2daece85230222e2996d51c5c
Paul <pauldro952@outlook.com>
2023-03-03 10:11:48 -0600

    FIX: show help data for --debug

--- app/Cmd/Help/SpreadsheetController.php
+++ app/Cmd/Help/SpreadsheetController.php
@@ -19,12 +19,14 @@ class SpreadsheetController extends Controller {
 		'report' => 'report=REPORT',
 		'id'     => 'id=ID',
 		'co'     => 'co=CO',
+		'debug'  => '--debug'
 	];
 	const OPTIONS_DEFINITIONS = [
 		// 'flag'   => 'Flag to attach',
 		'report' => 'Report Code / COBOL Program ID',
 		'id'     => 'Report ID',
 		'co'     => 'Company Number',
+		'debug'  => 'Run in debug? (displays debug info, file(s) location(s), execution duration)'
 	];
 	const SUBCOMMANDS = [];
 
